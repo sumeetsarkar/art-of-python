8c7a17de07553c3407f972e3d4f7565700474535
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-13 02:01:21 +0530

    added lambda sample

---
+++ src/lambda.py
@@ -0,0 +1,17 @@
+def multiplier(num):
+  return lambda a: a * num
+
+twice = multiplier(2)
+thrice = multiplier(3)
+
+print(twice(4))
+print(thrice(4))
+
+list = [1, 2, 3, 4, 5]
+
+result = map(lambda n: n * 2, list)
+for x in result:
+  print(x)
+
+for n in list:
+  print((lambda a: a * 2)(n))
