12b4763b3010b14b5266261f06463cee23598fc3
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-12 11:10:36 +0530

    args example

---
+++ src/args.py
@@ -0,0 +1,18 @@
+def printColors(*colors):
+  for color in colors:
+    if type(color) is dict:
+      print(color.get('key'), color.get('name'))
+    else:
+      print(color)
+
+printColors('voilet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red')
+
+printColors(
+  { 'key': 0, 'name': 'voilet' },
+  { 'key': 1, 'name': 'indigo' },
+  { 'key': 2, 'name': 'blue' },
+  { 'key': 3, 'name': 'green' },
+  { 'key': 4, 'name': 'yellow' },
+  { 'key': 5, 'name': 'orange' },
+  { 'key': 6, 'name': 'red' },
+)
