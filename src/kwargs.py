d930c80b902967e59738ae38d054b30d3bf9a2c8
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-12 11:12:56 +0530

    updated kwargs example

--- src/kwargs.py
+++ src/kwargs.py
@@ -3,3 +3,14 @@ def printArguments(**kwargs):
     print(k, v)
 
 printArguments(fname = 'Sumeet', lname = 'Sarkar')
+# outputs
+# fname Sumeet
+# lname Sarkar
+
+def printArguments2(fname, **kwargs):
+  for k,v in kwargs.items():
+    print(k, v)
+
+printArguments2(fname = 'Sumeet', lname = 'Sarkar')
+# outputs
+# lname Sarkar
