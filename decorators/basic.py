d691529f4819a39ae5d9d381b41606190fadd532
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-15 18:33:57 +0530

    decorator basic sample

---
+++ decorators/basic.py
@@ -0,0 +1,30 @@
+"""
+Demonstrates basic use case of decorators
+"""
+
+import time
+
+def add(a, b):
+  return a + b
+
+def compute_time(fn):
+  """Returns a wrapper function which can take arguments
+  and call decorated function with the arguments and add additional functionality to it
+  """
+  def wrapper(*args, **kwargs):
+    start = time.time()
+    fn(*args, **kwargs)
+    end = time.time()
+    print(fn.__name__, 'time elapsed: ', end - start)
+  return wrapper
+
+add = compute_time(add)
+add(1, 2)
+
+# or
+
+@compute_time
+def add_with_decorator(a, b):
+  return a + b
+
+add_with_decorator(1, 2)
