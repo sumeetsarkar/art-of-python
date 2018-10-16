6477baa57b24241468a052ca6c77723f6fbbe4a7
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-16 17:01:58 +0530

    context manager for file implementation

---
+++ context-managers/custom-file-cm.py
@@ -0,0 +1,25 @@
+"""
+Demonstrates a sample implementation of File Context Manager
+"""
+
+import os
+
+class FileCM:
+  def __init__(self, filePath, mode):
+    self.__filePath = filePath
+    self.__mode = mode
+  
+  def __enter__(self):
+    self.__f = open(self.__filePath, self.__mode)
+    return self.__f
+  
+  def __exit__(self, *args):
+    self.__f.close()
+
+DIR_NAME = 'data'
+FILE_NAME = 'sample.txt'
+FILE_PATH = os.path.join(os.path.dirname(__file__), DIR_NAME, FILE_NAME)
+
+with FileCM(FILE_PATH, 'r') as f:
+  for line in f.readlines():
+    print(line)
