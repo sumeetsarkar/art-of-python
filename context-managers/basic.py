f877723fbd4639591b1cc9b518aa5597de20b37f
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-16 14:37:23 +0530

    basic context manager sample

---
+++ context-managers/basic.py
@@ -0,0 +1,13 @@
+"""
+Demonstrates basic usage of an inbuilt context manager for File
+"""
+
+import os
+
+DIR_NAME = 'data'
+FILE_NAME = 'sample.txt'
+FILE_PATH = os.path.join(os.path.dirname(__file__), DIR_NAME, FILE_NAME)
+
+with open(FILE_PATH) as f:
+  for l in f.readlines():
+    print(l)
