90db3bda97e4af65729a4a84b13ce225a65728de
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-16 13:39:32 +0530

    file sample update

--- file-handling/file-io.py
+++ file-handling/file-io.py
@@ -3,14 +3,18 @@ import os
 def create_file_if_not_exists(filename):
   fileW = open(filename, 'a')
   fileW.write('\nThe quick brown fox jumped over the lazy dog')
+  fileW.close()
 
 def read_file(filename):
+  fileR = None
   try:
     fileR = open(filename, 'rt') # (rt) read in text mode is default
     for line in fileR:
       print(line)
   except:
     print('File does not exsits')
+  finally:
+    fileR.close()
 
 DIR = './data'
 FILE_NAME = 'sample.txt'
