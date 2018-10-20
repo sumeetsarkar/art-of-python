acbd1c98b80b6d33f0e7cdd3cb69d0e0f3affaf3
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-21 00:30:15 +0530

    yield from gen sample added

---
+++ generators/nested-gen.py
@@ -0,0 +1,24 @@
+def foo():
+  yield 'a'
+  yield 'b'
+  yield 'c'
+
+def bar():
+  yield foo()
+  yield 'd'
+  yield 'e'
+  yield 'f'
+
+def baz():
+  yield from foo()
+  yield 'd'
+  yield 'e'
+  yield 'f'
+
+print('yield foo()')
+for c in bar():
+  print(c)
+
+print('yield from foo()')
+for c in baz():
+  print(c)
