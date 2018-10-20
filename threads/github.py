62589b03440469fb7326764d39be0b12d079721c
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-21 01:22:16 +0530

    thread decorator sample to make http requests

--- threads/github.py
+++ threads/github.py
@@ -10,12 +10,15 @@ import json
 # https://api.github.com/search/repositories?sort=stars&order=desc&q=repo-to-search&page=n
 
 class Github:
+  
   SERVICE = 'https://api.github.com'
   ENDPOINT_SEARCH = '{}/search/repositories?sort=stars&order=desc&q={}&page={}'
+
 
   def __get_request_url(self, repoName, page = 1):
     repoName = urllib.parse.quote(repoName)
     return Github.ENDPOINT_SEARCH.format(Github.SERVICE, repoName, page)
+
 
   def __parse_response(self, response, encoding):
     print('\n')
@@ -29,6 +32,7 @@ class Github:
       print('stars: {0} forks: {1} issues: {2}'.format(str(repo['stargazers_count']), str(repo['forks']), str(repo['open_issues'])))
       print('----------------------------\n')
 
+
   def search_repo(self, repoName):
     myreq = request.Request(method='GET', url=self.__get_request_url(repoName))
     with request.urlopen(myreq) as r:
@@ -36,10 +40,19 @@ class Github:
       print('\nResponse Headers')
       print('----------------\n')
       print(r.headers)
-      self.__parse_response(r.read(), 'utf8')
+      response = r.read()
+      print('\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+      print('Displaying search results for: ' + repoName)
+      print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+      self.__parse_response(response, 'utf8')
 
 
-try:
-  Github().search_repo(sys.argv[1])
-except IndexError as e:
-  print(e)
+def main():
+  try:
+    Github().search_repo(sys.argv[1])
+  except IndexError as e:
+    print(e)
+
+
+if __name__ == '__main__':
+  main()
---
+++ threads/thread-decorator.py
@@ -0,0 +1,35 @@
+from threading import Thread
+from github import Github
+
+
+class ThreadedTask:
+  """Decorator class to run target functions in different threads
+  """
+  def __init__(self, fn):
+    self.__fn = fn
+
+  def __call__(self, *args):
+    t = Thread(target=self.__fn, args=args)
+    print(t.name)
+    t.start()
+
+
+# decorate func with ThreadedTask
+@ThreadedTask
+def make_http_call(repoName):
+  Github().search_repo(repoName)
+
+
+def main():
+  listOfRepos = [
+    'art-of-js',
+    'art-of-python',
+    'solarjs',
+  ]
+  print('Initiating threads to search for Github repositories...')
+  for repo in listOfRepos:
+    make_http_call(repo)
+  print('Requested...')
+
+if __name__ == '__main__':
+  main()
