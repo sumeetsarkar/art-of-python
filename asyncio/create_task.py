c18add0aab6a4f5338fe0960f83aa2200fc12924
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-21 04:17:15 +0530

    updated create_task sample with ensure_future

--- asyncio/create_task.py
+++ asyncio/create_task.py
@@ -8,6 +8,7 @@ import asyncio
 
 async def task(name):
   print('{} sleep for 1 sec'.format(name))
+  # sleep() always suspends the current task, allowing other tasks to run.
   await asyncio.sleep(1)
   print('{} sleep for 1 sec again'.format(name))
   await asyncio.sleep(2)
@@ -16,14 +17,19 @@ async def task(name):
 
 async def main():
   # The asyncio.create_task() function to run coroutines concurrently as asyncio Tasks
+  # In Python 3.7+
   taskA = asyncio.create_task(task('task A'))
   taskB = asyncio.create_task(task('task B'))
+
+  # This works in all Python versions but is less readable
+  taskC = asyncio.ensure_future(task('task C'))
 
   # await tasks taskA and taskB, interleaves operations in both the tasks
   await taskA
   await taskB
+  await taskC
 
-  print('Both tasks completed')
+  print('All tasks completed')
 
 
 asyncio.run(main())
