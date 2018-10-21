44887b281fe91a3b1264eadbcfbd1d23eb19a4dd
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-21 21:11:05 +0530

    ternary operator sample

---
+++ 101/ternary-operator.py
@@ -0,0 +1,18 @@
+"""
+Demonstrates 3 different ways to implement ternary operations
+"""
+
+
+a, b = 10, 20
+
+# explicit if else
+min = a if a < b else b
+print(min)
+
+# tuple index, true -> 1, false -> 0
+min = (b, a)[ a < b ]
+print(min)
+
+# dict, True, False keys
+min = { True:a, False:b } [ a < b ]
+print(min)
