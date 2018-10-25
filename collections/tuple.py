9e85b9bfd83752d63f7d38597f0244322e9b379d
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-25 15:49:53 +0530

    Tuple sample updated

--- collections/tuple.py
+++ collections/tuple.py
@@ -1,22 +1,38 @@
-"""
-Demonstrates handling of return values
+""" Demonstrates tuples in python
+        packing
+        unpacking
+        Methods:
+            count
+            index
 """
 
 import sys
 
-# tuple assingment
-justTuple = 1, 2, 3, 4
+# Tuple assingment
+justTuple = 1, 2, 3, 4, 4, 5, 6
 print(justTuple)
 
-# tuple spread
+# Tuple count function returns the number of occurences of given item
+print('Tuple count of 2:', justTuple.count(2))
+print('Tuple count of 4:', justTuple.count(4))
+print('Tuple count of 0:', justTuple.count(0))
+
+# index() Returns the first index of occurence of given item
+print('Index of 2:', justTuple.index(2))
+print('Index of 4:', justTuple.index(4))
+# print('Index of 0:', justTuple.index(0))    # raises ValueError: 0 not in tuple
+
+# Tuple spread
 justTuple1, justTuple2, justTuple3, justTuple4 = 1, 2, 3, 4
 print(justTuple1, justTuple2, justTuple3, justTuple4)
 
-# tuple iteration
+
+# Tuple iteration
 for n in justTuple:
     print(n)
 
 
+# A practical example using tuples to return multiple values
 def get_extremes(arr):
     minVal, maxVal = sys.maxsize, sys.maxsize * -1
     for n in arr:
@@ -28,7 +44,7 @@ def get_extremes(arr):
     return minVal, maxVal
 
 
-# tuple unpacked to individual variables
+# Tuple unpacked to individual variables
 # note: number of explicit members mentioned to unpack variables can be:
 # 1 -> recieves tuple
 # n, where n equal to tuple length
