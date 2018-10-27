1f1fd250ec8016e220fb09d299e2efc565ab1494
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-27 20:44:28 +0530

    simple python hands on computation for decimal to binary and vice versa

---
+++ 101/simple_decimal_binary.py
@@ -0,0 +1,26 @@
+"""
+Simple program to convert between binary and decimal
+"""
+
+def decimaltobinary(d):
+    binary = []
+    while d/2 != 0:
+        binary.append(str(d%2))
+        d = int(d/2)
+    binary.reverse()
+    return int(''.join(binary))
+    
+
+def binarytodecimal(b):
+    a = [s for s in str(b)]
+    a.reverse()
+    d = 0
+    for i in range(len(a)):
+        d += 2**i * int(a[i])
+    return d
+
+
+print(decimaltobinary(49))      # 110001
+print(binarytodecimal(110001))  # 49
+
+print(binarytodecimal(decimaltobinary(49)) == 49)   # True
